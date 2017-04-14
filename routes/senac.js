a6e84b0210d3bec35f850e1f89c199ef315c186c
Icaro Heimig <icaro.mh@gmail.com>
2017-04-14 15:27:07 -0300

    refactor: fix files as eslint config

--- routes/senac.js
+++ routes/senac.js
@@ -1,40 +1,35 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const Auth = require('../config');
+const HttpStatus = require('http-status-code');
 const NRP = require('../config/nrp');
 const UserModel = require('../models/user');
 
 const router = express.Router();
 
 router.post('/login', (req, res) => {
-  const {username, password, unity} = req.body;
+  const { username, password, unity } = req.body;
 
-  if(!username || !password || !unity){
-    res.sendStatus(Status.BadRequest);
+  if (!username || !password || !unity) {
+    res.sendStatus(HttpStatus.BAD_REQUEST);
     return;
   }
 
-  NRP.emit('worker:login', {username, password, unity});
-  res.sendStatus(200);
+  NRP.emit('worker:login', { username, password, unity });
+  res.sendStatus(HttpStatus.OK);
 });
 
 router.post('/sync', (req, res) => {
-
   UserModel.isSyncing(res.locals.token, (err, user) => {
-    if(err || !user){
-      res.send(400);
+    if (err || !user) {
+      res.send(HttpStatus.BAD_REQUEST);
       return;
     }
 
-    const {isSyncing, username, cookie} = user.senacCredentials
-    if(!isSyncing){
-      NRP.emit('worker:sync', {cookie, username});
-      res.sendStatus(200);
+    const { isSyncing, username, cookie } = user.senacCredentials;
+    if (!isSyncing) {
+      NRP.emit('worker:sync', { cookie, username });
+      res.sendStatus(HttpStatus.OK);
     }
-  })
-
-
-  // res.sendStatus(200);
+  });
 });
 
 
--- routes/worker/index.js
+++ routes/worker/index.js
@@ -1,19 +1,21 @@
 const UserModel = require('../../models/user');
 const NRP = require('../../config/nrp');
+const debug = require('debug')('api:worker');
 
-NRP.on('api:save-cookie', data => {
-  const query = {"senacCredentials.username": data.username};
+NRP.on('api:save-cookie', (data) => {
+  const query = { 'senacCredentials.username': data.username };
   const update = {
-    "$set":{
-      "senacCredentials.cookie": data.cookie
-    }
+    $set: {
+      'senacCredentials.cookie': data.cookie,
+    },
   };
 
-  UserModel.update(query, update, (err, d) => {
-    console.log(err, d);
-  })
+  UserModel.update(query, update, (err) => {
+    if (err) debug(err);
+    else debug(`Saved cookie for ${data.username}`);
+  });
 });
 
-NRP.on('api:save-classes', data => {
-  console.log(data);
+NRP.on('api:save-classes', (data) => {
+  debug(JSON.stringify(data));
 });
