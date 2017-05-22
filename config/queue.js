abfc9da11964e3e9624708da18e5a1505736a584
Icaro Heimig <icaro.mh@gmail.com>
2017-05-21 22:07:44 -0300

    fix: kue redis config

--- config/queue.js
+++ config/queue.js
@@ -2,8 +2,10 @@ const kue = require('kue');
 const REDIS = require('./index').redis;
 
 const config = {
-  host: REDIS.HOST,
-  port: REDIS.PORT,
+  redis: {
+    host: REDIS.HOST,
+    port: REDIS.PORT,
+  },
 };
 
 module.exports = kue.createQueue(config);
